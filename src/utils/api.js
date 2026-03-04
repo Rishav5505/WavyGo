74373301ad7e43708aabf2c62aba6f08d11e3b2b
Rishav Kumar <rishavkumar33372@gmail.com>
2026-03-04 17:52:21 +0530

    Updated backend API URL to Render endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API = axios.create({
     baseURL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
         ? 'http://localhost:5000/api'
-        : 'https://api.wavygo.com/api'
+        : 'https://wavygo.onrender.com/api'
 });
 
 // Add token to requests if available
